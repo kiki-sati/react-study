1f6a9bf61ebb78fe14ae9bb629ae9768bb9d9202
kimrumm <kimrumm@naver.com>
2022-12-04 22:50:45 +0900

    Feat:01-hello-react

--- 01-hello-react/src/MyComponent1.js
+++ 01-hello-react/src/MyComponent1.js
@@ -1,6 +1,8 @@
 //react 기본 패키지 참조 (필수)
 import React from "react";
 
+import MySubCompnent from './MySubComponet';
+
 /**
  * 함수형 컴포넌트 정의
  * - 함수 이름은 혼선을 방지하기 위해 소스파일 이름과 동일하게 구성하는 것이 일반적
@@ -10,6 +12,10 @@ const MyComponent1 = () => {
     <div>
         <h2>안녕하세요 리액트</h2>
         <p>반갑습니다.</p>
+
+        <MySubCompnent />
+        <MySubCompnent />
+        <MySubCompnent />
     </div>);
 }
 
--- 01-hello-react/src/MyComponent2.js
+++ 01-hello-react/src/MyComponent2.js
@@ -1,12 +1,19 @@
 //react 기본 패키지 참조 (필수)
 import React from "react";
 
-const MyComponent1 = () => {
+import MySubComponet from './MySubComponet'
+
+const MyComponent2 = () => {
     return (
     <div>
         <h2>에베베베베</h2>
         <p>반갑습니다.</p>
+
+        <MySubComponet />
+        <MySubComponet />
+        <MySubComponet />
+
     </div>);
 }
 
-export default MyComponent1;
+export default MyComponent2;
